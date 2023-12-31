8a0efdf46cccf2429059d3380ba49e5ebcead730
Noalino <bgelineau.dev@gmail.com>
2023-12-31 11:41:03 +0100

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Noalino on 31/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
